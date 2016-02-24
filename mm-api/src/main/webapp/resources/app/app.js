60637bf006163a07610f974990f174f2816e50ea
Latika <latika.bhurani@metacube.com>
2016-02-24 18:19:28 +0530

    Commit for #114279833 : dropdown menu

--- mm-api/src/main/webapp/resources/app/app.js
+++ mm-api/src/main/webapp/resources/app/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('micromaps', [ 'ngRoute', 'ngResource']);
+var app = angular.module('micromaps', [ 'ngRoute', 'ngResource', 'ngAnimate', 'ui.bootstrap']);
 
 app.config(['$routeProvider', function($routeProvider) {
            
